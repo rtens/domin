d15243370c2e654ffc446723348c2eb8ff989b24
Nikolas Martens <Nikolas.M@rtens.org>
2015-07-10 16:25:40 +0200

    Added first WebField: StringField

---
+++ spec/web/fields/StringFieldSpec.php
@@ -0,0 +1,49 @@
+<?php
+namespace spec\rtens\domin\web\fields;
+
+use rtens\domin\Parameter;
+use rtens\domin\web\fields\StringField;
+use rtens\scrut\tests\statics\StaticTestSuite;
+use watoki\reflect\type\IntegerType;
+use watoki\reflect\type\StringType;
+use watoki\reflect\type\UnknownType;
+
+class StringFieldSpec extends StaticTestSuite {
+
+    /** @var \rtens\domin\web\WebField */
+    public $field;
+
+    protected function before() {
+        $this->field = new StringField();
+    }
+
+    function handlesOnlyStringTypes() {
+        $this->assert($this->field->handles(new Parameter('foo', new StringType())));
+        $this->assert->not($this->field->handles(new Parameter('foo', new UnknownType())));
+        $this->assert->not($this->field->handles(new Parameter('foo', new IntegerType())));
+    }
+
+    function inflatesNonEmptyStrings() {
+        $this->assert($this->field->inflate('foo'), 'foo');
+        $this->assert($this->field->inflate(''), null);
+    }
+
+    function hasNoHeadElements() {
+        $this->assert->size($this->field->headElements(new Parameter('foo', new StringType())), 0);
+    }
+
+    function emptyField() {
+        $this->assert($this->field->render(new Parameter('foo', new StringType()), null),
+            '<input type="text" name="foo" value=""/>');
+    }
+
+    function requiredField() {
+        $this->assert($this->field->render(new Parameter('bar', new StringType(), true), null),
+            '<input type="text" name="bar" value="" required="true"/>');
+    }
+
+    function withValue() {
+        $this->assert($this->field->render(new Parameter('foo', new StringType()), 'some stuff'),
+            '<input type="text" name="foo" value="some stuff"/>');
+    }
+}
---
+++ src/web/fields/StringField.php
@@ -0,0 +1,48 @@
+<?php
+namespace rtens\domin\web\fields;
+
+use rtens\domin\Parameter;
+use rtens\domin\web\Element;
+use rtens\domin\web\WebField;
+use watoki\reflect\type\StringType;
+
+class StringField implements WebField {
+
+    /**
+     * @param \rtens\domin\Parameter $parameter
+     * @return bool
+     */
+    public function handles(Parameter $parameter) {
+        return $parameter->getType() instanceof StringType;
+    }
+
+    /**
+     * @param string $serialized
+     * @return mixed
+     */
+    public function inflate($serialized) {
+        return $serialized ?: null;
+    }
+
+    /**
+     * @param Parameter $parameter
+     * @return array|\rtens\domin\web\Element[]
+     */
+    public function headElements(Parameter $parameter) {
+        return [];
+    }
+
+    public function render(Parameter $parameter, $value) {
+        $attributes = [
+            "type" => "text",
+            "name" => $parameter->getName(),
+            "value" => $value
+        ];
+
+        if ($parameter->isRequired()) {
+            $attributes["required"] = 'true';
+        }
+
+        return (string) new Element("input", $attributes);
+    }
+}
