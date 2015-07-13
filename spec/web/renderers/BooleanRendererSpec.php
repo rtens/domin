8447680b31e618808c99965ac35ade826dd5f8dd
Nikolas Martens <Nikolas.M@rtens.org>
2015-07-13 09:00:27 +0200

    Added BooleanRenderer

---
+++ spec/web/renderers/BooleanRendererSpec.php
@@ -0,0 +1,28 @@
+<?php
+namespace spec\rtens\domin\web\renderers;
+
+use rtens\domin\web\renderers\BooleanRenderer;
+use rtens\scrut\tests\statics\StaticTestSuite;
+
+class BooleanRendererSpec extends StaticTestSuite {
+
+    /** @var BooleanRenderer */
+    private $renderer;
+
+    protected function before() {
+        $this->renderer = new BooleanRenderer();
+    }
+
+    function handlesBooleans() {
+        $this->assert($this->renderer->handles(true));
+        $this->assert($this->renderer->handles(false));
+        $this->assert->not($this->renderer->handles(1));
+        $this->assert->not($this->renderer->handles(0));
+        $this->assert->not($this->renderer->handles('foo'));
+    }
+
+    function rendersBooleans() {
+        $this->assert($this->renderer->render(true), 'Yes');
+        $this->assert($this->renderer->render(false), 'No');
+    }
+}
