abaab09b857e9d49af52ca1f737971b5876fbb83
Nikolas Martens <Nikolas.M@rtens.org>
2015-07-17 10:38:57 +0200

    Special Types cannot use MultiType since that breaks proper type analysis

--- src/reflection/TypeFactory.php
+++ src/reflection/TypeFactory.php
@@ -3,39 +3,31 @@ namespace rtens\domin\reflection;
 
 use watoki\reflect\ClassResolver;
 use watoki\reflect\Type;
+use watoki\reflect\type\StringType;
 
 class TypeFactory extends \watoki\reflect\TypeFactory {
 
-    /**
-     * @param array|string[] $hints
-     * @param \ReflectionClass $class
-     * @return Type
-     */
-    public function fromTypeHints(array $hints, \ReflectionClass $class) {
+    public function fromTypeHint($hint, \ReflectionClass $class) {
         $resolver = new ClassResolver($class);
 
-        foreach ($hints as $hint) {
+        if (strtolower(substr($hint, -3)) == '-id') {
+            $target = $resolver->resolve(substr($hint, 0, -3));
+            return new IdentifierType($target, new StringType());
 
-            if (strtolower(substr($hint, -3)) == '-id') {
-                $otherType = parent::fromTypeHints(array_diff($hints, [$hint]), $class);
-                $target = $resolver->resolve(substr($hint, 0, -3));
-                return new IdentifierType($target, $otherType);
+        } else if (strpos($hint, '::') && substr($hint, -1) == '_') {
+            list($container, $constant) = explode('::', $hint);
+            $reflection = new \ReflectionClass($resolver->resolve($container));
 
-            } else if (strpos($hint, '::') && substr($hint, -1) == '_') {
-                list($container, $constant) = explode('::', $hint);
-                $reflection = new \ReflectionClass($resolver->resolve($container));
-
-                $options = [];
-                foreach ($reflection->getConstants() as $name => $value) {
-                    if (substr($name, 0, strlen($constant)) == $constant) {
-                        $options[] = $value;
-                    }
+            $options = [];
+            foreach ($reflection->getConstants() as $name => $value) {
+                if (substr($name, 0, strlen($constant)) == $constant) {
+                    $options[] = $value;
                 }
-
-                $otherType = parent::fromTypeHints(array_diff($hints, [$hint]), $class);
-                return new EnumerationType($options, $otherType);
             }
+
+            return new EnumerationType($options, new StringType());
         }
-        return parent::fromTypeHints($hints, $class);
+
+        return parent::fromTypeHint($hint, $class);
     }
 }
