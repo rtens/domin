e5e27ce5db76740a749c73bfda07a4813081f3be
Nikolas Martens <Nikolas.M@rtens.org>
2015-07-25 20:57:02 +0200

    Display action description in overview

--- src/delivery/web/root/IndexResource.php
+++ src/delivery/web/root/IndexResource.php
@@ -84,6 +84,7 @@ class IndexResource extends Container {
         foreach ($this->actions->getAllActions() as $id => $action) {
             $actions[] = [
                 'caption' => $action->caption(),
+                'description' => explode("\n\n", $action->description())[0],
                 'link' => ['href' => $base->appended($id)->toString()]
             ];
         }
