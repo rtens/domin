6d499c899ff249a0855748d4d481ad3aa2e23ac1
Nikolas Martens <Nikolas.M@rtens.org>
2015-07-26 11:04:08 +0200

    Refactored MobileDetector

--- src/delivery/web/MobileDetector.php
+++ src/delivery/web/MobileDetector.php
@@ -2,30 +2,18 @@
 namespace rtens\domin\delivery\web;
 
 use Detection\MobileDetect;
-use watoki\factory\Factory;
 
 class MobileDetector {
-
-    /** @var MobileDetect */
-    private $detect;
-
-    /**
-     * @param Factory $factory <-
-     */
-    function __construct(Factory $factory) {
-        if (class_exists(MobileDetect::class)) {
-            $this->detect = $factory->getInstance(MobileDetect::class);
-        }
-    }
 
     /**
      * @return bool
      */
     public function isMobile() {
-        if (!$this->detect) {
-            return true;
+        if (class_exists(MobileDetect::class)) {
+            $detect = new MobileDetect();
+            return $detect->isMobile() || $detect->isTablet();
         }
-        return $this->detect->isMobile() || $this->detect->isTablet();
+        return true;
     }
 
 }
