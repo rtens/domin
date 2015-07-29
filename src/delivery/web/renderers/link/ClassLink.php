f8ef586a54ffd36dfd6186a627350ee4b3d9974e
Nikolas Martens <Nikolas.M@rtens.org>
2015-07-29 10:32:37 +0200

    Missing doc comment

--- src/delivery/web/renderers/link/ClassLink.php
+++ src/delivery/web/renderers/link/ClassLink.php
@@ -3,6 +3,11 @@ namespace rtens\domin\delivery\web\renderers\link;
 
 class ClassLink extends GenericLink {
 
+    /**
+     * @param string $class
+     * @param string $actionId
+     * @param callable|null $parameters
+     */
     public function __construct($class, $actionId, callable $parameters = null) {
         parent::__construct($actionId, function ($object) use ($class) {
             return is_a($object, $class);
